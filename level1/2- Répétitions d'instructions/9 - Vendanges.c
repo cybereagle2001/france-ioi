240549509d04580cf4819d151edbc3a856ac59fd
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-01-20 22:24:17 +0100

    Create 9 - Vendanges.c

---
+++ level1/2- Répétitions d'instructions/9 - Vendanges.c
@@ -0,0 +1,21 @@
+//coded by cybereagle2001
+#include <stdio.h>
+#include <robot.h>
+#define repeat(nb) for (int _loop=1 , _max = nb; _loop<= _max; _loop++)
+
+int main()
+{
+   repeat(20)
+   {
+      ramasser();
+      repeat(15)
+      {
+         droite();
+      }
+      deposer();
+      repeat(15)
+      {
+         gauche();
+      }   
+   }
+}
