d60bd28b0cb61e9040a3107413826fadc28e0e03
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-01-20 22:34:28 +0100

    Create 7 - Progresser par l'erreur.c

---
+++ level1/3 - Calculs et découverte des variables/7 - Progresser par l'erreur.c
@@ -0,0 +1,13 @@
+//coded by cybereagle2001
+#include <stdio.h>
+
+int main()
+{
+   printf("V\n");
+   printf("V\n");
+   printf("I\n");
+   printf("I\n");
+   printf("V\n");
+   printf("I\n");
+   printf("I\n");
+}
