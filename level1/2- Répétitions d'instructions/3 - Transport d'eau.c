e19ccdb3016e517ed00ee094395659d3e9d1d660
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-01-20 22:14:43 +0100

    Create 3 - Transport d'eau.c

---
+++ level1/2- Répétitions d'instructions/3 - Transport d'eau.c
@@ -0,0 +1,18 @@
+// coded by cybereagle2001
+# include <stdio.h>
+#define repeat(nb) for (int _loop=1, _max=nb; _loop<=_max; _loop++)
+
+int main()
+{
+    repeat(2)
+    {
+        gauche();
+    }
+    printf("Bonjour, laissez-moi vous aider\n");
+    ramasser();
+    repeat(32)
+    {
+        droite();
+    }
+    deposer();
+}
