2bedef8bd6dba0a7fd630c1ef251a18f3d3d514a
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-01-20 21:58:34 +0100

    Create 3- plan de la montagne.c

---
+++ level1/1 – Affichage de texte, suite d'instructions/3- plan de la montagne.c
@@ -0,0 +1,10 @@
+// coded by cybereagle2001
+#include <stdio.h>
+{
+    printf("Tout droit tu grimperas,\n");
+    printf("La clé tu trouveras,\n");
+    printf("Habile tu seras,\n");
+    printf("Quand tu les porteras,\n");
+    printf("Et avec le chef tu reviendras !\n");
+    return 0;
+}
