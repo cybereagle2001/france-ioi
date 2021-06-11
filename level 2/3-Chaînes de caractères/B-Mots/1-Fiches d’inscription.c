98a170dbb76ec5010cf941205242d802b5dc2949
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-06-11 16:59:23 +0100

    Update 1-Fiches d’inscription.c

--- level 2/3-Chaînes de caractères/B-Mots/1-Fiches d’inscription.c
+++ level 2/3-Chaînes de caractères/B-Mots/1-Fiches d’inscription.c
@@ -1,1 +1,18 @@
+//coded by cybereagle2001
 
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+   int nbDePersonne;
+   char nom[100];
+   char prenom[100];
+   scanf("%d",&nbDePersonne);
+   for (int loop=0; loop<nbDePersonne;loop++)
+   {
+      scanf("%s %s\n",prenom,nom);
+      printf("%s %s\n",nom,prenom);
+   }
+   
+}
