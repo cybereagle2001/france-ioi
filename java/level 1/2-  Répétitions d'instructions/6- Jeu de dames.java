60aa7e46058c17e0a59de2a5494f6de364f1ec29
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-04 20:39:11 +0100

    Create 6- Jeu de dames.java

---
+++ java/level 1/2-  Répétitions d'instructions/6- Jeu de dames.java
@@ -0,0 +1,24 @@
+/** coded by cybereagle2001 */
+
+class Main{
+   public static void main(String[] args){
+      for (int loop=0; loop<10; loop++){
+         for (int j=0; j<10; j++){
+            System.out.print("OXOX");
+         }
+         System.out.println("");
+         for (int i=0; i<10; i++){
+            System.out.print("XOXO");
+         }
+         System.out.println("");
+         for (int x=0; x<10; x++){
+            System.out.print("OXOX");
+         }
+         System.out.println("");
+         for (int y=0; y<10; y++){
+            System.out.print("XOXO");
+         }
+         System.out.println("");
+      }
+   }
+}
