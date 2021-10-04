39b927d1118e337c2a5666969695f664f479cfdb
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-04 16:17:26 +0100

    Create 1- Punition.java

---
+++ java/level 1/2-  Répétitions d'instructions/1- Punition.java
@@ -0,0 +1,9 @@
+/** coded by cybereagle2001*/
+
+class Main{
+   public static void main(String[] args){
+      for (int loop=0; loop< 135; loop++){
+         System.out.println("Je dois respecter le Grand Sorcier.");
+      }
+   }
+}
