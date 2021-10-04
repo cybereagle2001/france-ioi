ad3fc88806b616dff36778f96160ca1d61ae6eaf
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-04 16:10:21 +0100

    Create 6-Recette secrète.java

---
+++ java/level 1/1- Affichage de texte, suite d'instructions/6-Recette secrète.java
@@ -0,0 +1,17 @@
+/** coded by cybereagle2001 */
+import static algorea.Robot.*;
+
+class Main {
+   public static void main(String[] args) {
+      remplir(5);
+      transferer(5, 3);
+      vider(5);
+      transferer(3, 5);
+      remplir(3);
+      transferer(3, 5);
+      vider(5);
+      transferer(3, 5);
+      remplir(3);
+      transferer(3, 5);
+   }
+}
