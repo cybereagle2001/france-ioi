cd5e6d18380fb820d19676e1607d18a72b89f6e3
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-04 14:38:34 +0100

    Create 2- présentation.java

---
+++ java/level 1/1- Affichage de texte, suite d'instructions/2- présentation.java
@@ -0,0 +1,9 @@
+/** coded by cybereagle2001 */
+
+class Main{
+    public static void main(String[] args){
+        System.out.println("Coucou !");
+        System.out.println("Je m'appelle Camthalion");
+        System.out.println("Ma devise est 'Parler peu mais parler bien'.");
+    }
+}
