16f0f12d9a8c22d5400cb980ac9f043a61ba797c
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-04 16:38:20 +0100

    Create 2- Mathématiques de base.java

---
+++ java/level 1/2-  Répétitions d'instructions/2- Mathématiques de base.java
@@ -0,0 +1,9 @@
+/** coded by cybereagle2001*/
+
+class Main {
+   public static void main(String[] args) {
+      for (int loop = 0; loop < 13; loop = loop + 1) {
+         System.out.println("9 * 8 = 72");
+      }
+   }
+}
