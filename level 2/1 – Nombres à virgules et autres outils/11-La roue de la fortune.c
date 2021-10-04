da3d65541f8bea5c07387a706beec94f5c41fbb4
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-04 14:17:33 +0100

    Delete level 2 directory

--- level 2/1 – Nombres à virgules et autres outils/11-La roue de la fortune.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-  int nbZones;
-  scanf("%d", &nbZones);
-  printf("%d", ((nbZones % 24) + 24 ) % 24);
-}
--- level 2/1 – Nombres à virgules et autres outils/2-Conversions de distances.c
+++
@@ -1,9 +0,0 @@
-//coded by cybereagle2001
-
-#include <stdio.h>
-
-void main(){
-    double number;
-    scanf("%lf",&number);
-    printf("%lf",number/0.707);
-}
