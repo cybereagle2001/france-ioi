acb051a73641474f1d49ab8c5e92b4fa35518659
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-04 14:53:52 +0100

    Create 4) Dans le fourré.java

---
+++ java/level 1/1- Affichage de texte, suite d'instructions/4) Dans le fourré.java
@@ -0,0 +1,15 @@
+/** coded by cybereagle2001 */
+import static algorea.Robot.*;
+
+class Main{
+   public static void main(String[] args){
+      haut();
+      haut();
+      haut();
+      droite();
+      droite();
+      bas();
+      bas();
+      droite();
+   }
+}
