c84f362106f6365eaf5d115be749f50a291e2359
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-04 16:59:04 +0100

    Create 4-Le secret du Goma.java

---
+++ java/level 1/2-  Répétitions d'instructions/4-Le secret du Goma.java
@@ -0,0 +1,14 @@
+/** coded by cybereagle2001*/
+
+import static algorea.Robot.*;
+
+class Main{
+   public static void main(String[] args){
+      for (int loop=0; loop<15; loop++){
+         droite();
+         ramasser();
+      }
+      droite();
+      deposer();
+   }
+}
