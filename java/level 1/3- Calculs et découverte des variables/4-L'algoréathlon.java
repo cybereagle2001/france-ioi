10c8b0eb8b9b0383e7bb6066b64132dd4f5f644c
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-06 17:35:56 +0100

    Create 4-L'algoréathlon.java

---
+++ java/level 1/3- Calculs et découverte des variables/4-L'algoréathlon.java
@@ -0,0 +1,12 @@
+/** coded by cybereagle2001*/
+
+class Main{
+   public static void main(String[] args){
+      int x= 2+34+6;
+      System.out.print(x);
+      System.out.print(" ");
+      System.out.print(2*x);
+      System.out.print(" ");
+      System.out.print(3*x);
+   }
+}
