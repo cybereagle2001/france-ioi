48e9a2c4ed67163a08ae1b763f4789131caac713
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-07 15:06:23 +0100

    Create 6- Une partie de cache-cache.java

---
+++ java/level 1/3- Calculs et découverte des variables/6- Une partie de cache-cache.java
@@ -0,0 +1,10 @@
+/** coded by cybereagle2001*/
+
+class Main{
+   public static void main(String[] args){
+      for (int loop=1; loop<=100;loop++){
+         System.out.println(loop);
+      }
+      System.out.println("J'arrive !");
+   }
+}
