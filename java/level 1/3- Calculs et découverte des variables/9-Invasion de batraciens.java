e02ddf12c71d7e6b5f9975c000f729a9420cc743
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-07 16:13:11 +0100

    Create 9-Invasion de batraciens.java

---
+++ java/level 1/3- Calculs et découverte des variables/9-Invasion de batraciens.java
@@ -0,0 +1,11 @@
+/** coded by cybereagle2001*/
+
+class Main{
+   public static void main(String[] args){
+      int nombre=1337;
+      for (int loop=0; loop<12; loop++){
+         nombre=nombre*2;
+      }
+      System.out.println(nombre);
+   }
+}
