e5176c15ec30cd75a1a6b96b35d2386686434c3c
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-07 16:02:52 +0100

    Create 8-Décollage de fusée.java

---
+++ java/level 1/3- Calculs et découverte des variables/8-Décollage de fusée.java
@@ -0,0 +1,10 @@
+/** coded by cybereagle2001*/
+
+class Main{
+   public static void main(String[] args){
+      for (int loop=100; loop>=0;loop--){
+         System.out.println(loop);
+      }
+      System.out.print("Décollage !");
+   }
+}
