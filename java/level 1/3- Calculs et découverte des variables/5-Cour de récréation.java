4f9a0e779480fda8ae91cd28d0ec28751aa93bd1
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-07 15:00:03 +0100

    Create 5-Cour de récréation.java

---
+++ java/level 1/3- Calculs et découverte des variables/5-Cour de récréation.java
@@ -0,0 +1,14 @@
+/** coded by cybereagle2001*/
+
+class Main{
+   public static void main(String[] args){
+      int first=17;
+      int second=7;
+      int third=5;
+      int fourth=2;
+      // 5 fois le premier bâton plus 2 fois le second plus 1 fois le troisième plus 2 fois le quatrième. 
+      int cote= 5*first+2*second+third+2*fourth;
+      System.out.println(cote*cote);
+      System.out.println(cote*4);
+   }
+}
