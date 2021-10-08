791004c4b4bfa3766f8c3a976fa00afd40f090a8
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-08 16:08:31 +0100

    Create 10- Kermesse.java

---
+++ java/level 1/3- Calculs et découverte des variables/10- Kermesse.java
@@ -0,0 +1,11 @@
+/** coded by cybereagle2001*/
+
+class Main{
+   public static void main(String[] args){
+      int value=0;
+      for (int loop=1; loop<=50; loop++){
+         System.out.println(value+loop);
+         value= value+loop;
+      }
+   }
+}
