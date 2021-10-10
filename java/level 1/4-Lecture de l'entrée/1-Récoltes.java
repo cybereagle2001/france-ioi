9fbd3cccd7cb38dbb43b2a3f3ec3d1ec128a48a3
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-10 13:03:29 +0100

    Create 1-Récoltes.java

---
+++ java/level 1/4-Lecture de l'entrée/1-Récoltes.java
@@ -0,0 +1,11 @@
+/**  coded by cybereagle2001*/
+
+import algorea.Scanner;
+
+class Main{
+   static Scanner input= new Scanner(System.in);
+   public static void main(String[] args){
+      int number= input.nextInt();
+      System.out.print(number*number*23);
+   }
+}
