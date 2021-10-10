cdb49d8e5ce9e566d51d01d6a426068e3375fb96
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-10 11:47:24 +0100

    Create 13-Table de multiplication.java

---
+++ java/level 1/3- Calculs et découverte des variables/13-Table de multiplication.java
@@ -0,0 +1,12 @@
+/** coded by cybereagle2001*/
+
+class Main{
+   public static void main(String[] args){
+      for(int loop=1; loop<=20; loop++){
+         for (int j=1; j<=20; j++){
+            System.out.print(loop*j+" ");
+         }
+         System.out.println("");
+      }
+   }
+}
