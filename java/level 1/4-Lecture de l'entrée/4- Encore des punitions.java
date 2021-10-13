56e89935d5c732b76a436ffe7941a8404e6ae678
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-10-13 18:43:29 +0100

    Create 4- Encore des punitions.java

---
+++ java/level 1/4-Lecture de l'entrée/4- Encore des punitions.java
@@ -0,0 +1,13 @@
+/**coded by cybereagle2001*/
+
+import algorea.Scanner;
+
+class Main{
+   static Scanner input= new Scanner(System.in);
+   public static void main(String[] args){
+      int nombre= input.nextInt();
+      for (int loop=0; loop<nombre;loop++){
+         System.out.println("Je dois suivre en cours");
+      }
+   }
+}
